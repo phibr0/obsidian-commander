f7da2ec92ca75d1443d0336b5a231eecc3f93140
Phillip Bronzel <bronzel.phillip@gmail.com>
2022-11-29 21:09:24 +0100

    fix: fix #45

--- src/ui/components/Accordion.tsx
+++ src/ui/components/Accordion.tsx
@@ -4,9 +4,12 @@ import { ObsidianIcon } from "src/util";
 
 interface AccordionProps extends ComponentProps<"details"> {
 	title: string;
-	children?: h.JSX.Element | h.JSX.Element[];
+	children: h.JSX.Element | h.JSX.Element[];
 }
-export default function Accordion({ title, children }: AccordionProps): h.JSX.Element {
+export default function Accordion({
+	title,
+	children,
+}: AccordionProps): h.JSX.Element {
 	const [open, setOpen] = useState(false);
 
 	const toggleHandler = (): void => {
@@ -16,10 +19,17 @@ export default function Accordion({ title, children }: AccordionProps): h.JSX.El
 	return (
 		<div className="cmdr-accordion cmdr-sep-con" aria-expanded={open}>
 			<div className="cmdr-accordion-header" onClick={toggleHandler}>
-				<ObsidianIcon className="cmdr-accordion-chevron clickable-icon" icon="chevron-down" size={24} />
+				<ObsidianIcon
+					className="cmdr-accordion-chevron clickable-icon"
+					icon="chevron-down"
+					size={24}
+				/>
 				<span>{title}</span>
 			</div>
-			<div className="cmdr-accordion-content">
+			<div
+				className="cmdr-accordion-content"
+				style={{ maxHeight: [children].flat().length * 120 + "px" }}
+			>
 				{children}
 			</div>
 		</div>
